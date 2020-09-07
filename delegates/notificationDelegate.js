30ec58839db0245cbb4e3a95ea1ffebc8b29f38c
alvoscares <alvoscares@gmail.com>
2020-09-06 22:07:50 -0300

    asdfasdf

--- delegates/notificationDelegate.js
+++ delegates/notificationDelegate.js
@@ -23,61 +23,50 @@ const createNotification = async (notificationData) => {
 //SendEmail
 
 const oEmail = new email({
-  "host":"smtp.gmail.com",
-  "port":"465",
-  "secure":true,
-  "auth":{
-      "type":"login",
-      "user":"weraisen.test@gmail.com",
-      "pass":"alvo1234"
+  "host": "smtp.gmail.com",
+  "port": "465",
+  "secure": true,
+  "auth": {
+    "type": "login",
+    "user": "weraisen.test@gmail.com",
+    "pass": "alvo1234"
   }
 });
 
 
 const sendEmail = async (emailData) => {
-console.log(emailData);
+  console.log(emailData);
   var textoMail = ``;
   var asuntoMail = "";
-  switch(emailData.origen) {
+  switch (emailData.origen) {
 
     //Usuario aprobado
     case 1:
-      textoMail = `¡Hola! Tu usuario fue aprobado con éxito. Estamos felices de darte la bienvenida a nuestra comunidad.
-      Al presionar el siguiente link ya podrás disfrutar de nuestros servicios:
-      http://localhost:3000/login
-      ¡Muchas gracias! El equipo de WeRaisen.`
-
+      textoMail = 'tmp_mails/tmp_usuario_aprobado.html'
       asuntoMail = "¡Usuario de WER aprobado!"
       break;
-      //Usuario denegado
+    //Usuario denegado
     case 2:
-      textoMail = `¡Hola! No pudimos validar tu usuario. Había inconsistencias en los datos ingresados. Si crees que hubo un error, volvé a intentarlo con el siguiente link:
-      http://localhost:3000/signup
-      ¡Muchas gracias! El equipo de WeRaisen.`
-
-      asuntoMail = "Usuario de WER denegado"      
+      textoMail = 'tmp_mails/tmp_usuario_denegado.html'
+      asuntoMail = "Usuario de WER denegado"
       break;
     //Restablecer contraseña
-      case 3:
-        const userIdResp = await userDelegate.findUserByEmail(emailData.emailDestino)
-        const idToSendEmail = userIdResp._id
+    case 3:
+      const userIdResp = await userDelegate.findUserByEmail(emailData.emailDestino)
+      const idToSendEmail = userIdResp._id
 
-        textoMail = `Hola! Sabemos que perdiste tu contraseña de WeRaisen. ¡Lo sentimos por eso!
+      textoMail = `Hola! Sabemos que perdiste tu contraseña de WeRaisen. ¡Lo sentimos por eso!
         ¡Pero no te preocupes! Podes usar el siguiente link para restablecer tu contraseña: Si no usas este enlace dentro de las 3 horas, caducará. 
         http://localhost:3000/ChangePassword/${idToSendEmail}        
         Gracias. El equipo de WeRaisen.`
 
-      asuntoMail = "Restablecé tu contraseña"      
+      asuntoMail = "Restablecé tu contraseña"
       break;
 
-      //Cuenta bloqueada
-      case 4:
-        textoMail = `Hola! Lamentamos comunicarte que tu cuenta de WeRaisen ha sido bloqueada ya que consideramos que infringiste nuestras normas comunitarias.
-        
-        
-        Saludos, el equipo de WeRaisen.`
-
-      asuntoMail = "Cuenta bloqueada"      
+    //Cuenta bloqueada
+    case 4:
+      textoMail = 'tmp_mails/tmp_cuenta_bloqueada.html'
+      asuntoMail = "Cuenta bloqueada"
       break;
 
   }
@@ -85,9 +74,8 @@ console.log(emailData);
   let email = {
     from: "weraisen.test@gmail.com",
     to: emailData.emailDestino,
-    subject:  asuntoMail,
-    // html: textoMail,
-    html: {path: 'tmp_mails/index.html'},
+    subject: asuntoMail,    
+    html: { path: textoMail },
   }
   oEmail.enviarCorreo(email);
   // res.send("ok");  
